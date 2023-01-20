9c88fa16bb158b494ae73f87d680b9aee5245c6d
0xHaku <takatwoin0510@gmail.com>
2023-01-20 21:48:24 +0900

    add components for parameter

--- packages/user-app/pages/create-airdrop.tsx
+++ packages/user-app/pages/create-airdrop.tsx
@@ -2,9 +2,13 @@ import { useMetaMask } from "metamask-react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Popover from "@mui/material/Popover";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Head from "next/head";
 import { useCallback, useState } from "react";
@@ -12,6 +16,14 @@ import { useCallback, useState } from "react";
 export default function CreateAirdrop() {
   const { status, connect, account, chainId, ethereum } = useMetaMask();
   const { switchChain } = useMetaMask();
+
+  function shortenAddress(address: string | null) {
+    if (address === null) {
+      return "";
+    } else {
+      return address.substring(0, 6) + "..." + address.substring(38);
+    }
+  }
 
   function ChainButton() {
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
@@ -52,6 +64,10 @@ export default function CreateAirdrop() {
     function NetworksButtonList() {
       return (
         <List>
+          <ListItem key={-1}>
+            <Typography>Switch Network</Typography>
+          </ListItem>
+          <Divider />
           {Object.keys(networks).map((key, index) => {
             return (
               <ListItem key={index}>
@@ -132,13 +148,129 @@ export default function CreateAirdrop() {
         {status === "connected" ? (
           <>
             <AppBarStatus />
-            <Box
-              sx={{ p: 1, m: 1, display: "flex", justifyContent: "center" }}
-            ></Box>
+            <Stack>
+              <Box
+                sx={{
+                  p: 1,
+                  m: 2,
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Stack
+                  sx={{
+                    mt: 1,
+                    width: 0.6,
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      m: 2,
+                    }}
+                  >
+                    Airdrop Token Address
+                  </Typography>
+                  <TextField id="airdrop-token" variant="outlined" />
+                  <Typography
+                    sx={{
+                      m: 2,
+                    }}
+                  >
+                    Snapshot Token Address
+                  </Typography>
+                  <Grid
+                    container
+                    sx={{
+                      mt: 1,
+                    }}
+                    columnSpacing={{ xs: 2 }}
+                  >
+                    <Grid item xs={8}>
+                      <TextField
+                        fullWidth
+                        label="address"
+                        id="snapshot-token-address-1"
+                        variant="outlined"
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <TextField
+                        fullWidth
+                        label="coefficient"
+                        id="snapshot-token-coefficient-1"
+                        variant="outlined"
+                        defaultValue="1"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    sx={{
+                      mt: 1,
+                    }}
+                    columnSpacing={{ xs: 2 }}
+                  >
+                    <Grid item xs={8}>
+                      <TextField
+                        fullWidth
+                        label="address"
+                        id="snapshot-token-address-2"
+                        variant="outlined"
+                      />
+                    </Grid>
+                    <Grid item xs={4}>
+                      <TextField
+                        fullWidth
+                        label="coefficient"
+                        id="snapshot-token-coefficient-2"
+                        variant="outlined"
+                        defaultValue="1"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Typography
+                    sx={{
+                      m: 2,
+                    }}
+                  >
+                    Snapshot Block Number
+                  </Typography>
+                  <TextField
+                    id="snapshot-block-number"
+                    variant="outlined"
+                    sx={{
+                      width: 0.2,
+                    }}
+                  />
+                  <Typography
+                    sx={{
+                      m: 2,
+                    }}
+                  >
+                    Excluded Address List(Separated by commas or line breaks)
+                  </Typography>
+                  <TextField
+                    id="excluded-address-list"
+                    variant="outlined"
+                    multiline
+                  />
+                </Stack>
+              </Box>
+              <Box
+                sx={{
+                  p: 1,
+                  m: 2,
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Button variant="contained">Create Airdrop List</Button>
+              </Box>
+            </Stack>
           </>
         ) : (
-          <Box sx={{ p: 1, m: 1, display: "flex", justifyContent: "center" }}>
-            <Button variant="contained" onClick={connect} sx={{ p: 1, m: 1 }}>
+          <Box sx={{ p: 1, m: 2, display: "flex", justifyContent: "center" }}>
+            <Button variant="contained" onClick={connect} sx={{ p: 1, m: 2 }}>
               Connect to MetaMask
             </Button>
           </Box>
@@ -146,12 +278,4 @@ export default function CreateAirdrop() {
       </>
     </>
   );
-}
-
-function shortenAddress(address: string | null) {
-  if (address === null) {
-    return "";
-  } else {
-    return address.substring(0, 6) + "..." + address.substring(38);
-  }
 }
