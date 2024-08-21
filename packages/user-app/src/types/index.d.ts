3f60f786385624a8bef2aa22fdb66274e0b44145
takadr <t4k4.cr@gmail.com>
2024-08-21 12:48:46 +0900

    break workspace structure and add ccip compatible distributor contracts

--- packages/user-app/src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-type OnChangeEvent = React.ChangeEvent<HTMLInputElement>;
-type holdersResponseData = { address: string; balance: string }[];
-type holdersResponse = { data: holdersResponseData };
-type numberResponse = { data: BigNumber };
-type AddressMap = { [chainId: string]: string };
-type airdropListData = { address: string; amount: BigNumber };
-interface Window {
-    ethereum: any
-}
